d5535aabba200588128c92c2dadf640421c77c57
Dogron <fedor.ryzhenkov@gmail.com>
2019-01-13 13:38:32 +0300

    Revert ""Empty" text instead of empty string"
    This reverts commit 1418348

--- Assets/Scripts/ImportantScripts/Dialogue.cs
+++ Assets/Scripts/ImportantScripts/Dialogue.cs
@@ -91,7 +91,7 @@ namespace ImportantScripts
 				CanvasManager.CanvasManagerIn.DialogueTextNode.text = "";
 				foreach (var t in CanvasManager.CanvasManagerIn.DialogueTextAnswer)
 				{
-					t.text = "empty text";
+					t.text = "";
 				}
 			}
 		}
